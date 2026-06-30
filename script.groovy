c6d5c5deecfab44dfc2a2a449f62eb2a9a280310
rafaelyanez <rafaelyanez@gmail.com>
2026-06-30 13:50:25 -0400

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -7,6 +7,8 @@ def createImage() {
   echo "building docker image..."
   withCredentials([usernamePassword(credentialsId: 'docker-credentials', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
   sh 'docker build -t 165.227.47.37:8083/java-app:1.0 .'
+  sh 'echo $PASSWORD'
+  sh 'echo $USERNAME'
   sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin 165.227.47.37:8083'
   sh 'docker push 165.227.47.37:8083/java-app:1.0'
   }
